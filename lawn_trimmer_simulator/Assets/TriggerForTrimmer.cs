d8b55d4f1e003492dc5b5fe93a573f15213319ca
Nikon Chigoya <nikon.chigoya63@gmail.com>
2024-05-22 04:14:20 +0300

    adding a trigger for the mower

---
+++ lawn_trimmer_simulator/Assets/TriggerForTrimmer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class TriggerForTrimmer : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            // Проверяем, является ли объект, вошедший в триггер, объектом пользователя
+            if (other.gameObject.name == "FirstPersonController")
+            {
+                Debug.Log("Пользователь вошел в триггер");
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            // Проверяем, является ли объект, вышедший из триггера, объектом пользователя
+            if (other.gameObject.name == "FirstPersonController")
+            {
+                Debug.Log("Пользователь вышел из триггера");
+            }
+        }
+    }
+
+}
